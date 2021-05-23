fb412edb19b3d12c19ec4c426c601b2fec6c596c
pavitra <83263125+pavitradalawai@users.noreply.github.com>
2021-05-23 11:25:38 +0530

    Add files via upload

---
+++ AlphaE.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class AlphaE {  
+public static void main(String args[]){     
+ 
+ Scanner sc = new Scanner(System.in);
+          System.out.println("enter the  number");
+         
+        int row = sc.nextInt();
+
+for(int i=1;i<=row;i++){    
+
+for(int j=1 ;j<=row;j++){  
+  
+if  (i == 1|| j == 1 || i == (row/2)+1 || i == 5)
+
+System.out.print(i + "" + j + " ");
+
+else 
+System.out.print("   ");       
+}    
+System.out.println();
+}    
+}} 
